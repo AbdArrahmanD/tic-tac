1a644e01681caa0d2f7eb6328517362cb601c5dd
AbdArrahmanD <abd.first.no2@gmail.com>
2021-11-02 13:17:26 +0200

    add attack strategies to the game

--- lib/models/game_logic.dart
+++ lib/models/game_logic.dart
@@ -85,8 +85,60 @@ class Game {
     // {0,1,2
     //  3,4,5
     //  6,7,8}
+// attack
 
-    if (Player.playerX.containsAll(0, 1) && emptyCells.contains(2)) {
+    if (Player.playerO.containsAll(0, 1) && emptyCells.contains(2)) {
+      index = 2;
+    } else if (Player.playerO.containsAll(1, 2) && emptyCells.contains(0)) {
+      index = 0;
+    } else if (Player.playerO.containsAll(3, 4) && emptyCells.contains(5)) {
+      index = 5;
+    } else if (Player.playerO.containsAll(4, 5) && emptyCells.contains(3)) {
+      index = 3;
+    } else if (Player.playerO.containsAll(6, 7) && emptyCells.contains(8)) {
+      index = 8;
+    } else if (Player.playerO.containsAll(7, 8) && emptyCells.contains(6)) {
+      index = 6;
+    } else if (Player.playerO.containsAll(0, 2) && emptyCells.contains(1)) {
+      index = 1;
+    } else if (Player.playerO.containsAll(3, 5) && emptyCells.contains(4)) {
+      index = 4;
+    } else if (Player.playerO.containsAll(6, 8) && emptyCells.contains(7)) {
+      index = 7;
+    } else if (Player.playerO.containsAll(0, 3) && emptyCells.contains(6)) {
+      index = 6;
+    } else if (Player.playerO.containsAll(1, 4) && emptyCells.contains(7)) {
+      index = 7;
+    } else if (Player.playerO.containsAll(2, 5) && emptyCells.contains(8)) {
+      index = 8;
+    } else if (Player.playerO.containsAll(6, 3) && emptyCells.contains(0)) {
+      index = 0;
+    } else if (Player.playerO.containsAll(7, 4) && emptyCells.contains(1)) {
+      index = 1;
+    } else if (Player.playerO.containsAll(8, 5) && emptyCells.contains(2)) {
+      index = 2;
+    } else if (Player.playerO.containsAll(6, 0) && emptyCells.contains(3)) {
+      index = 3;
+    } else if (Player.playerO.containsAll(7, 1) && emptyCells.contains(4)) {
+      index = 4;
+    } else if (Player.playerO.containsAll(8, 2) && emptyCells.contains(5)) {
+      index = 5;
+    } else if (Player.playerO.containsAll(0, 4) && emptyCells.contains(8)) {
+      index = 8;
+    } else if (Player.playerO.containsAll(2, 4) && emptyCells.contains(6)) {
+      index = 6;
+    } else if (Player.playerO.containsAll(6, 4) && emptyCells.contains(2)) {
+      index = 2;
+    } else if (Player.playerO.containsAll(8, 4) && emptyCells.contains(0)) {
+      index = 0;
+    } else if (Player.playerO.containsAll(2, 6) && emptyCells.contains(4)) {
+      index = 4;
+    } else if (Player.playerO.containsAll(8, 0) && emptyCells.contains(4)) {
+      index = 4;
+    }
+
+// Defense
+    else if (Player.playerX.containsAll(0, 1) && emptyCells.contains(2)) {
       index = 2;
     } else if (Player.playerX.containsAll(1, 2) && emptyCells.contains(0)) {
       index = 0;
